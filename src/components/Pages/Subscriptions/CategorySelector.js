02ec47d3dd017d3dc960af5a943458b3c7c8fb05
Rémi Champlon <remi.champlon@vold.lu>
2023-07-28 22:02:49 +0200

    add image

--- src/components/Pages/Subscriptions/CategorySelector.js
+++ src/components/Pages/Subscriptions/CategorySelector.js
@@ -1,6 +1,7 @@
 import H2 from "@/components/H2";
 import BlockButton from "@/components/BlockButton";
 import React from "react";
+import { API_ROUTE } from "@/api/api";
 
 function CategorySelector(props) {
     return (<div>
@@ -10,8 +11,8 @@ function CategorySelector(props) {
                 key={category.id}
                 onClick={() => props.setSelectedCategory(category)}>
                 <img
-                    className="h-24 w-24 bg-primary"
-                    alt={category.title}
+                    className="h-24 w-24 text-primary" src={API_ROUTE + category.medias[0]?.path}
+                    alt={category.medias[0]?.title}
                 />
                 {category.title}
             </BlockButton>))}
